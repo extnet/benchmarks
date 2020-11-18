09a2d3d10422e2ae5c02ed9c146b148580a1e77c
Andrey Chechel <andrey@object.net>
2020-11-18 14:24:40 +0300

    Benchmark for Ext.Net.Classic (.NET 5.0)

---
+++ Ext.Net.Benchmarks.Classic5/Controllers/BenchmarkController.cs
@@ -0,0 +1,43 @@
+﻿using Ext.Net.Benchmarks.Classic5.Models;
+using Ext.Net.Benchmarks.Common;
+using Ext.Net.Core;
+
+using Microsoft.AspNetCore.Mvc;
+
+namespace Ext.Net.Benchmarks.Classic5.Controllers
+{
+    public class BenchmarkController : Controller
+    {
+        private static readonly TestRunner _testRunner = new TestRunner();
+
+        public IActionResult Index()
+        {
+            return this.View();
+        }
+
+        public IActionResult Grid(int? count = null, string test = null)
+        {
+            _testRunner.LogTestRun(test);
+
+            var model = new GridBenchmarkModel
+            {
+                GridCount = count ?? 1
+            };
+
+            return this.View(model);
+        }
+
+        public IActionResult RenderDirectToast(int? count)
+        {
+            var total = count ?? 1;
+            var X = this.X();
+
+            for (int i = 0; i < total; i++)
+            {
+                X.Toast("Test Message " + i);
+            }
+
+            return this.Direct();
+        }
+    }
+}
---
+++ Ext.Net.Benchmarks.Classic5/Startup.cs
@@ -0,0 +1,63 @@
+using Ext.Net.Core;
+
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace Ext.Net.Benchmarks.Classic5
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllersWithViews();
+            services.AddExtNet();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Benchmark/Error");
+            }
+
+            app.UseStaticFiles();
+
+            app.UseExtNetResources(cfg =>
+            {
+                cfg.UseEmbedded();
+                cfg.UseThemeSpotless();
+            });
+
+            app.UseRouting();
+
+            app.UseExtNet(cfg =>
+            {
+                cfg.Theme = ThemeKind.Spotless;
+                cfg.DisableAntiforgery = true;
+            });
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Benchmark}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
